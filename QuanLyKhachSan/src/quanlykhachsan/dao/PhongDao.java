6aa03d8ab90234cf78180c9f4344adc731013a68
ngocphung <ngocphung207c@gmail.com>
2019-05-31 23:13:30 +0700

    Sắp xếp lại folder - Tuấn Anh

---
+++ QuanLyKhachSan/src/quanlykhachsan/dao/PhongDao.java
@@ -0,0 +1,172 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package quanlykhachsan;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.hibernate.Query;
+import org.hibernate.Session;
+
+/**
+ *
+ * @author Phung
+ */
+public class PhongDao {
+    Session session = null;
+    List<Phong> phongList;
+    public PhongDao() {
+        this.session = HibernateUtil.getSessionFactory().openSession();
+    }
+
+    @SuppressWarnings("unchecked")
+    public List<Phong> getPhongList() {
+        phongList = new ArrayList<Phong>();
+        org.hibernate.Transaction tx = null;
+        try {
+            tx = session.beginTransaction();
+            Query q = session.createQuery("from Phong");
+            phongList = (List<Phong>) q.list();
+            tx.commit();
+        } catch (Exception e) {
+            phongList = null;
+            tx.rollback();
+            e.printStackTrace();
+        }finally {
+            //session.close();
+        }
+        return phongList;
+    }
+    
+    @SuppressWarnings("unchecked")
+     public List<Phong> getPhongTrongList() {
+        phongList = new ArrayList<Phong>();
+        org.hibernate.Transaction tx = null;
+        try {
+            tx = session.beginTransaction();
+            Query q = session.createQuery("from Phong where TinhTrang = 'Trong' ");
+            phongList = (List<Phong>) q.list();
+            tx.commit();
+        } catch (Exception e) {
+            phongList = null;
+            tx.rollback();
+            e.printStackTrace();
+        }finally {
+            //session.close();
+        }
+        return phongList;
+    }
+     
+     @SuppressWarnings("unchecked")
+     public List<Phong> timPhongByTen(String Ten) {
+        phongList = new ArrayList<Phong>();
+        org.hibernate.Transaction tx = null;
+        try {
+            tx = session.beginTransaction();
+            Query q = session.createQuery("from Phong where TenPhong = '" + Ten + "'");
+            phongList = (List<Phong>) q.list();
+            tx.commit();
+        } catch (Exception e) {
+            phongList = null;
+            tx.rollback();
+            e.printStackTrace();
+        }finally {
+           // session.close();
+        }
+        return phongList;
+    }
+     @SuppressWarnings("unchecked")
+     public List<Phong> timPhongTrongByTen(String Ten) {
+        phongList = new ArrayList<Phong>();
+        org.hibernate.Transaction tx = null;
+        try {
+            tx = session.beginTransaction();
+            Query q = session.createQuery("from Phong where TinhTrang = 'Trong' and TenPhong = '" + Ten + "'");
+            phongList = (List<Phong>) q.list();
+            tx.commit();
+        } catch (Exception e) {
+            phongList = null;
+            tx.rollback();
+            e.printStackTrace();
+        }finally {
+           // session.close();
+        }
+        return phongList;
+    }
+     
+     @SuppressWarnings("unchecked")
+     public List<Phong> timPhongByLoai(int Loai) {
+        phongList = new ArrayList<Phong>();
+        org.hibernate.Transaction tx = null;
+        try {
+            tx = session.beginTransaction();
+            Query q = session.createQuery("from Phong where MaLoai = " + Loai);
+            phongList = (List<Phong>) q.list();
+            tx.commit();
+        } catch (Exception e) {
+            phongList = null;
+            tx.rollback();
+            e.printStackTrace();
+        }finally {
+           // session.close();
+        }
+        return phongList;
+    }
+     @SuppressWarnings("unchecked")
+     public List<Phong> timPhongTrongByLoai(int Loai) {
+        phongList = new ArrayList<Phong>();
+        org.hibernate.Transaction tx = null;
+        try {
+            tx = session.beginTransaction();
+            Query q = session.createQuery("from Phong where TinhTrang = 'Trong' and MaLoai = " + Loai);
+            phongList = (List<Phong>) q.list();
+            tx.commit();
+        } catch (Exception e) {
+            phongList = null;
+            tx.rollback();
+            e.printStackTrace();
+        }finally {
+           // session.close();
+        }
+        return phongList;
+    }
+     @SuppressWarnings("unchecked")
+     public List<Phong> timPhongByLoaiTen(String Ten, int Loai) {
+        phongList = new ArrayList<Phong>();
+        org.hibernate.Transaction tx = null;
+        try {
+            tx = session.beginTransaction();
+            Query q = session.createQuery("from Phong where MaLoai = " + Loai + " and TenPhong = '" + Ten + "'");
+            phongList = (List<Phong>) q.list();
+            tx.commit();
+        } catch (Exception e) {
+            phongList = null;
+            tx.rollback();
+            e.printStackTrace();
+        }finally {
+            //session.close();
+        }
+        return phongList;
+    }
+     
+     @SuppressWarnings("unchecked")
+     public List<Phong> timPhongTrongByLoaiTen(String Ten, int Loai) {
+        phongList = new ArrayList<Phong>();
+        org.hibernate.Transaction tx = null;
+        try {
+            tx = session.beginTransaction();
+            Query q = session.createQuery("from Phong where TinhTrang = 'Trong' and MaLoai = " + Loai + " and TenPhong = '" + Ten + "'");
+            phongList = (List<Phong>) q.list();
+            tx.commit();
+        } catch (Exception e) {
+            phongList = null;
+            tx.rollback();
+            e.printStackTrace();
+        }finally {
+            //session.close();
+        }
+        return phongList;
+    }
+}
